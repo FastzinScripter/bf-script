597f305356b3fe7905491174ea01a613d473b1d4
FastzinScripter <fastzin.adm@gmail.com>
2025-08-09 11:27:10 -0300

    Update otimi-fruit-graper.lua

--- otimi-fruit-graper.lua
+++ otimi-fruit-graper.lua
@@ -1,1 +1,14 @@
 
+
+
+
+task.spawn(function() 
+  getgenv().Key = "MARU-KURZM-58WO-91JP-WEODI-GSG4G"
+  getgenv().id = "904871270397648947"
+  getgenv().Script_Mode = "Kaitun_Script"
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
+end)
+
+task.spawn(function() 
+  _G.Config = { UserID = "081cfa79-2618-42ec-ab9a-7ec1d42ab32a", discord_id = "904871270397648947" , Note = "Pc", } loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/main/trackstatblox"))()
+end)
