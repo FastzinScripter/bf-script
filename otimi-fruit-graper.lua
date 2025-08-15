cb4868c2c4d2b339cc90ec2028081e41ba0d75fc
FastzinScripter <fastzin.adm@gmail.com>
2025-08-15 02:52:29 -0300

    Update otimi-fruit-graper.lua

--- otimi-fruit-graper.lua
+++ otimi-fruit-graper.lua
@@ -1,108 +1,39 @@
-
-repeat task.wait() until game:IsLoaded()
-repeat task.wait() until game.Players
-repeat task.wait() until game.Players.LocalPlayer
-repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
-_G.Team = "Pirate" -- Marine / Pirate
-getgenv().Script_Mode = "Kaitun_Script"
-_G.MainSettings = {
-    ["EnabledHOP"] = true,
-    ['FPSBOOST'] = true,
-    ["FPSLOCKAMOUNT"] = 10,
-    ['WhiteScreen'] = false,
-    ['CloseUI'] = false,
-    ["NotifycationExPRemove"] = true,
-    ['AFKCheck'] = 150,
-    ["LockFragments"] = 0,
-    ["LockFruitsRaid"] = {
-        [1] = "Dough-Dough",
-        [2] = "Dragon-Dragon",        
-    }
-}
-_G.SharkAnchor_Settings = {
-    ["Enabled_Farm"] = false,
-    ['FarmAfterMoney'] = 2500000
-}
-_G.Quests_Settings = {        
-    ['Rainbow_Haki'] = false,
-    ["MusketeerHat"] = false,
-    ["PullLever"] = false,
-    ['DoughQuests_Mirror'] = {
-        ['Enabled'] = true,
-        ['UseFruits'] = true
-    }        
-}
-_G.Races_Settings = {
-    ['Race'] = {
-        ['EnabledEvo'] = false,
-        ["v2"] = false,
-        ["v3"] = false,
-        ["Races_Lock"] = {
-            ["Races"] = {
-                ["Mink"] = false,
-                ["Human"] = false,
-                ["Fishman"] = false
-            },
-            ["RerollsWhenFragments"] = 20000
-        }
-    }
-}
-_G.Fruits_Settings = {
-    ['Main_Fruits'] = {'Dough-Dough'},
-    ['Select_Fruits'] = {"Flame-Flame", "Ice-Ice", "Quake-Quake", "Light-Light", "Dark-Dark", "Spider-Spider", "Rumble-Rumble", "Magma-Magma", "Buddha-Buddha"}
-}
-_G.Settings_Melee = {
-    ['Superhuman'] = true,
-    ['DeathStep'] = true,
-    ['SharkmanKarate'] = true,
-    ['ElectricClaw'] = true,
-    ['DragonTalon'] = true,
-    ['Godhuman'] = true
-}
-_G.SwordSettings = {
-    ['Saber'] = true,
-    ["Pole"] = false,
-    ['MidnightBlade'] = false,
-    ['Shisui'] = true,
-    ['Saddi'] = true,
-    ['Wando'] = false,
-    ['Yama'] = true,
-    ['Rengoku'] = false,
-    ['Canvander'] = false,
-    ['BuddySword'] = false,
-    ['TwinHooks'] = false,
-    ['HallowScryte'] = false,
-    ['TrueTripleKatana'] = false,
-    ['CursedDualKatana'] = true
-}
-_G.GunSettings = {
-    ['Kabucha'] = false,
-    ['SerpentBow'] = false,
-    ['SoulGuitar'] = false
-}
-_G.FarmMastery_Settings = {
-    ['Melee'] = false,
-    ['Sword'] = false,
-    ['DevilFruits'] = false,
-    ['Select_Swords'] = {
-        ["AutoSettings"] = true,
-        ["ManualSettings"] = {
-            "Saber",
-            "Buddy Sword"
-        }
-    }
-}
-_G.Hop_Settings = {
-    ["Find Tushita"] = false
-}
--- Script Here !!!
-
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 
 task.spawn(function() 
-  getgenv().Key = "MARU-KURZM-58WO-91JP-WEODI-GSG4G"
-  getgenv().id = "904871270397648947"
-  getgenv().Script_Mode = "Kaitun_Script"
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
+    getgenv().Key = "b3ebb456b4d905cba3a7aad5"
+    getgenv().Setting = {
+        ["Delay Hop"] = 5,
+        ["Webhook"] = {
+            ["url"] = "",
+            ["Webhook Target Fruit"] = true,
+            ["Webhook Store Fruit"] = {
+                ["Rarity"] = {
+                    ["Mythical"] = true,
+                    ["Legendary"] = true, 
+                    ["Rare"] = false,
+                    ["Uncommon"] = false,
+                    ["Common"] = false,
+                },
+                ["Enabled"] = true, 
+            },
+            ["Webhook When Attack Factory"] = true,
+            ["Webhook When Attack Raid Castle"] = true,
+            ["Ping Discord"] = {
+                ["Enabled"] = false, 
+                ["Id Discord/Everyone"] = ""
+            },
+            ["Enabled"] = false,
+        },
+        ["Auto Random Fruit"] = true,
+        ["Use Portal Teleport"] = false,
+        ["Attacking"] = {
+            ["Weapon"] = "Melee", -- Sword,Melee,Blox Fruit
+            ["Raid Castle"] = true,
+            ["Factory"] = true,
+        }
+    }
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/HopFruit.lua"))()
 end)
 
 task.spawn(function() 
